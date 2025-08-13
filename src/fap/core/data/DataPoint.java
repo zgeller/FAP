5bf405cd086fd3ba656d3655974df87e72e6ac37
zgeller <gellerz@gmail.com>
2025-08-13 19:36:20 +0200

    Update DataPoint.java
    javadoc update

--- src/fap/core/data/DataPoint.java
+++ src/fap/core/data/DataPoint.java
@@ -31,12 +31,12 @@ public class DataPoint implements Serializable, Comparable<DataPoint> {
     private static final long serialVersionUID = 1L;
 
     /**
-     * The x coordinate of the data point (usually denotes time).
+     * The x-coordinate of the data point (usually denotes time).
      */
     private double x;
 
     /**
-     * The y coordinate of the data point (usually denotes value).
+     * The y-coordinate of the data point (usually denotes value).
      */
     private double y;
 
@@ -59,8 +59,8 @@ public class DataPoint implements Serializable, Comparable<DataPoint> {
     /**
      * Constructs a new data point with the given coordinates.
      * 
-     * @param x the value of the x coordinate
-     * @param y the value of the y coordinate
+     * @param x the value of the x-coordinate
+     * @param y the value of the y-coordinate
      */
     public DataPoint(double x, double y) {
         this.setX(x);
@@ -68,36 +68,36 @@ public class DataPoint implements Serializable, Comparable<DataPoint> {
     }
 
     /**
-     * Returns the x coordinate of this data point.
+     * Returns the x-coordinate of this data point.
      * 
-     * @return the X coordinate of this data point
+     * @return the x-coordinate of this data point
      */
     public double getX() {
         return x;
     }
     
     /**
-     * Sets the x coordinate of this data point.
+     * Sets the x-coordinate of this data point.
      * 
-     * @param x the value of the x coordinate
+     * @param x the value of the x-coordinate
      */
     public void setX(double x) {
         this.x = x;
     }
 
     /**
-     * Returns the y coordinate of this data point.
+     * Returns the y-coordinate of this data point.
      * 
-     * @return the y coordinate of this data point
+     * @return the y-coordinate of this data point
      */
     public double getY() {
         return y;
     }
 
     /**
-     * Sets the y coordinate of this data point.
+     * Sets the y-coordinate of this data point.
      * 
-     * @param y the value of the y coordinate
+     * @param y the value of the y-coordinate
      */
     public void setY(double y) {
         this.y = y;
@@ -113,12 +113,12 @@ public class DataPoint implements Serializable, Comparable<DataPoint> {
 
     /**
      * Compares this data point with the specified data point based on the time
-     * component (x coordinate).
+     * component (x-coordinate).
      * 
      * @param dp the data point to which this data point should be compared
-     * @return a negative integer, zero, or a positive integer as the x coordinate
-     *         of this data point is less than, equal to, or greater than the x
-     *         coordinate of {@code dp}
+     * @return a negative integer, zero, or a positive integer as the x-coordinate
+     *         of this data point is less than, equal to, or greater than the
+     *         x-coordinate of {@code dp}
      */
     @Override
     public int compareTo(DataPoint dp) {

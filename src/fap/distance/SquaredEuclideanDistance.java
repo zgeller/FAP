d1b15445a627718c30c01380f3598c09b4fceed0
zgeller <gellerz@gmail.com>
2025-08-24 12:45:58 +0200

    added the squred Euclidean distance measure

---
+++ src/fap/distance/SquaredEuclideanDistance.java
@@ -0,0 +1,91 @@
+/*   
+ * Copyright 2025 Zoltán Gellér
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fap.distance;
+
+import fap.core.data.TimeSeries;
+import fap.exception.IncomparableTimeSeriesException;
+
+/**
+ * Squared Euclidean distance measure. Time series must be the same length (n):
+ *
+ * <blockquote> <img src="doc-files/SquaredEuclideanDistance-1.png"> </blockquote>
+ * 
+ * @author Zoltán Gellér
+ * @version 2025.08.24.
+ * @see AbstractCopyableDistance
+ */
+public class SquaredEuclideanDistance extends AbstractCopyableDistance {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructs a new squared Euclidean distance measure.
+     */
+    public SquaredEuclideanDistance() {
+    }
+
+    /**
+     * Constructs a new squared Euclidean distance measure and sets whether to store
+     * distances.
+     * 
+     * @param storing {@code true} if storing distances should be enabled
+     */
+    public SquaredEuclideanDistance(boolean storing) {
+        super(storing);
+    }
+
+    /**
+     * @throws IncomparableTimeSeriesException if the time series are not the same
+     *                                         length
+     */
+    @Override
+    public double distance(TimeSeries series1, TimeSeries series2) {
+
+        // try to recall the distance
+        Double recall = this.recall(series1, series2);
+        if (recall != null)
+            return recall;
+        
+        int len = IncomparableTimeSeriesException.checkLength(series1, series2);
+
+        double distance = 0;
+
+        for (int i = 0; i < len; i++) {
+            
+            double y1 = series1.getY(i);
+            double y2 = series2.getY(i);
+
+            double tmp = y1 - y2;
+            distance += tmp * tmp;
+            
+        }
+        
+        // save the distance into the memory
+        this.store(series1, series2, distance);
+        
+        return distance;
+
+    }
+
+    @Override
+    public Object makeACopy(boolean deep) {
+        SquaredEuclideanDistance copy = new SquaredEuclideanDistance();
+        init(copy, deep);
+        return copy;
+    }
+    
+}

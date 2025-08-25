002942af24eac8ee727826f37522e14ef2d11d98
zgeller <gellerz@gmail.com>
2025-08-25 19:37:43 +0200

    Update SoergelDistance.java

--- src/fap/distance/SoergelDistance.java
+++ src/fap/distance/SoergelDistance.java
@@ -21,13 +21,12 @@ import fap.exception.IncomparableTimeSeriesException;
 import fap.util.MathUtils;
 
 /**
- * Soergel (Jaccard) distance measure. Time series must be the same length (n):
+ * Soergel (Jaccard) distance measure. Time series must be the same length (n)
+ * and they should be non-negative:
  *
  * <blockquote> <img src="doc-files/SoergelDistance-1.png"> </blockquote>
  * 
  * <ul>
- *  <li> <b>If not all elements of A and B are non-negative, the result may be
- *       negative.</b>
  *  <li> {@code 0/0} is treated as {@code 0} (see [2]).
  *  <li> Zero denominator is replaced by {@link MathUtils#getZeroDenominator()} 
  *       (see [2]).

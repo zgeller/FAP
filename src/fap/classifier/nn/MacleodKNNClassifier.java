f50e51a56d8348209a13ba94f40c0e0513d29c3b
zgeller <gellerz@gmail.com>
2026-08-14 14:41:20 +0200

    Update MacleodKNNClassifier.java

--- src/fap/classifier/nn/MacleodKNNClassifier.java
+++ src/fap/classifier/nn/MacleodKNNClassifier.java
@@ -60,7 +60,7 @@ import fap.util.ThreadUtils;
  * </ol>
  * 
  * @author Zoltán Gellér
- * @version 2026.04.21.
+ * @version 2026.08.14.
  * @see KNNClassifier
  */
 public class MacleodKNNClassifier extends KNNClassifier {
@@ -272,7 +272,7 @@ public class MacleodKNNClassifier extends KNNClassifier {
 
             LinkedDistanceNode<TimeSeries> node = firstNode;
 
-            Map<Double, Integer> neighbours = new HashMap<Double, Integer>();
+            Map<Double, Integer> neighbours = new HashMap<>();
 
             bestLabel = node.obj.getLabel();
             int bestWeight = 1;
@@ -302,7 +302,7 @@ public class MacleodKNNClassifier extends KNNClassifier {
 
             LinkedDistanceNode<TimeSeries> node = firstNode;
 
-            Map<Double, Double> neighbours = new HashMap<Double, Double>();
+            Map<Double, Double> neighbours = new HashMap<>();
 
             double alpha = this.getAlpha();
 
